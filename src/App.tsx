8dc17cf24a805abe00f5ad5c40337c45ee6322e9
Azur Demirovic <abelnijeumro@gmail.com>
2026-04-19 12:39:05 +0200

    header

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-white p-4 md:p-6 font-univers">
+      <header className="w-full">
+        <h1 className="text-[1.3rem] sm:text-[2.1rem] md:text-[3.4rem] lg:text-[5.6rem] leading-[0.9] tracking-tighter">
+          I AM MATEJ HANŽEL,<br />
+          A VISUAL DESIGNER WITH<br />
+          EXPIRIENCE IN PHOTOGRAPHY,<br />
+          MIXING LIGHT AND MOTION.
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
